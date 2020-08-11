48e8fd34045d843b4c445ceb423abd57b730fff1
dimomite <br.button.system@gmail.com>
2020-08-11 17:26:55 +0200

    Added slf4j for PC app.

--- v1/soft/PcApp/app/src/main/java/org/dimomite/brbuttonsystem/Main.kt
+++ v1/soft/PcApp/app/src/main/java/org/dimomite/brbuttonsystem/Main.kt
@@ -1,10 +1,23 @@
 package org.dimomite.brbuttonsystem
 
+import org.apache.logging.log4j.LogManager
+import org.slf4j.LoggerFactory
+
 fun main(args: Array<String>) {
+    configureLogging()
+
     println("Stating BrButtonSystem control PC app")
 
     val app = MainApp()
     app.doLaunch(args)
 
+
     println("App is shutting down")
 }
+
+private fun configureLogging() {
+    val l = LogManager.getRootLogger()
+
+    LoggerFactory.getLogger("appmain").debug("Logging from main(): DBG")
+}
+

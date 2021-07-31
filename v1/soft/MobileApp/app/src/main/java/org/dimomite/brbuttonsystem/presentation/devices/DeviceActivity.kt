41a50df2a6f256e74c93f54c6f7ad959db6d5e91
dimomite <dkuzkz@gmail.com>
2021-08-01 01:44:58 +0200

    Added PIP mode for DeviceActivity. Moslty for prototyping. Main PIP activity should be for running game.

--- v1/soft/MobileApp/app/src/main/java/org/dimomite/brbuttonsystem/presentation/devices/DeviceActivity.kt
+++ v1/soft/MobileApp/app/src/main/java/org/dimomite/brbuttonsystem/presentation/devices/DeviceActivity.kt
@@ -1,14 +1,72 @@
 package org.dimomite.brbuttonsystem.presentation.devices
 
+import android.app.PictureInPictureParams
+import android.app.RemoteAction
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import org.dimomite.brbuttonsystem.GlobalConfigs
 import org.dimomite.brbuttonsystem.R
+import org.dimomite.brbuttonsystem.data.system.SettingsRepository
+import org.dimomite.brbuttonsystem.domain.common.DataContainer
+import org.dimomite.brbuttonsystem.domain.common.convertDataContainer
+import timber.log.Timber
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class DeviceActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var settRepo: SettingsRepository
+
+    @Inject
+    lateinit var globalConfigs: GlobalConfigs
+
+    private val subs = CompositeDisposable()
+    private var isPipEnabled: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_device)
+
+        if (globalConfigs.pictureInPictureAvailable) {
+            subs.add(settRepo.provider().outFlow().map { convertDataContainer(it) { sett -> sett.isPipControlEnabled } }.distinctUntilChanged().subscribe({
+                isPipEnabled = it.exec(object : DataContainer.Visitor<Boolean, Boolean> {
+                    override fun visitOk(v: DataContainer.Ok<Boolean>): Boolean = v.data
+                    override fun visitPending(v: DataContainer.Pending<Boolean>): Boolean = false
+                    override fun visitError(v: DataContainer.Error<Boolean>): Boolean = false
+                })
+                Timber.i("DBG: isPipEnabled: $isPipEnabled")
+            }, {
+                Timber.w("Error in data setting flow")
+                isPipEnabled = false
+            }))
+        }
+    }
+
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+
+        if (globalConfigs.pictureInPictureAvailable) {
+            if (isPipEnabled) {
+                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+                    val maxActionsCount = maxNumPictureInPictureActions
+                    Timber.i("DBG: onUserLeaveHint(): before entering pip mode: max actions: $maxActionsCount")
+                    val params = PictureInPictureParams.Builder()
+                        .setActions(listOf()) // TODO
+                        .build()
+                    enterPictureInPictureMode(params)
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        subs.clear()
+        super.onDestroy()
     }
 
 }
--- v1/soft/MobileApp/app/src/main/java/org/dimomite/brbuttonsystem/presentation/devices/DevicesFragment.kt
+++ v1/soft/MobileApp/app/src/main/java/org/dimomite/brbuttonsystem/presentation/devices/DevicesFragment.kt
@@ -5,12 +5,47 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import org.dimomite.brbuttonsystem.R
 import org.dimomite.brbuttonsystem.databinding.FragmentDevicesBinding
+import org.dimomite.brbuttonsystem.databinding.RemoteDeviceListItemBinding
+import org.dimomite.brbuttonsystem.presentation.connection.RemoteDeviceUiModel
+import timber.log.Timber
 
 class DevicesFragment : Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val bindings = FragmentDevicesBinding.inflate(inflater, container, false)
+        val adapter = DevicesAdapter(inflater)
+        bindings.devicesList.adapter = adapter
 
         return bindings.root
     }
+
+    private data class DevicesViewHolder(val bindings: RemoteDeviceListItemBinding) : RecyclerView.ViewHolder(bindings.root)
+
+    private inner class DevicesAdapter(private val inflater: LayoutInflater) : RecyclerView.Adapter<DevicesViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DevicesViewHolder {
+            val bindings = RemoteDeviceListItemBinding.inflate(inflater, parent, false)
+            bindings.root.setOnClickListener {
+                Timber.i("DBG: Navigate to device activity")
+                findNavController().navigate(R.id.action_devicesFragment_to_deviceActivity)
+            }
+            return DevicesViewHolder(bindings)
+        }
+
+        override fun onBindViewHolder(holder: DevicesViewHolder, position: Int) {
+            holder.bindings.model = RemoteDeviceUiModel("Device #$position", "SN: #1234")
+        }
+
+        override fun getItemCount(): Int {
+            return 2
+        }
+    }
+
+
 }
+
+
+
